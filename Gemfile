5482ebeb8a3f3842a4344f514d7ebd1fb7b0bc09
Toropon <duck7quick@gmail.com>
2018-01-15 16:04:32 +0900

    add puma info/add Procfile

--- Gemfile
+++ Gemfile
@@ -5,7 +5,6 @@ source 'https://rubygems.org'
 gem 'rails', '~> 5.0.0'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3', group: [:development, :test]
-gem 'pg', group: :production
 # Use Puma as the app server
 gem 'puma', '~> 3.0'
 # Use SCSS for stylesheets
@@ -41,6 +40,10 @@ group :development do
   gem 'web-console'
 end
 
+group :production do
+  gem 'pg'
+  gem 'rails_12factor', '0.0.2'
+end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
@@ -86,5 +86,5 @@ Rails.application.configure do
 
   # user added!
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  
+
 end
