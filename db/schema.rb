8af7610730503f81dc9529255b30cd0cea3c3c7e
Toropon <duck7quick@gmail.com>
2018-01-23 08:30:59 +0900

    update puma settings

--- db/schema.rb
+++ db/schema.rb
@@ -13,145 +13,145 @@
 ActiveRecord::Schema.define(version: 20160923101211) do
 
   create_table "accounts", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_accounts_on_email", unique: true
     t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
   end
 
   create_table "answers", force: :cascade do |t|
-    t.integer  "question_id"
-    t.text     "content"
-    t.text     "name"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.integer "question_id"
+    t.text "content"
+    t.text "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "blogconfigs", force: :cascade do |t|
-    t.text     "title"
-    t.text     "subtitle"
-    t.text     "stylename"
+    t.text "title"
+    t.text "subtitle"
+    t.text "stylename"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "bloggenres", force: :cascade do |t|
-    t.text     "name"
-    t.text     "memo"
+    t.text "name"
+    t.text "memo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "blogposts", force: :cascade do |t|
-    t.text     "title"
-    t.text     "read"
-    t.text     "content"
-    t.integer  "bloggenre_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.text "title"
+    t.text "read"
+    t.text "content"
+    t.integer "bloggenre_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "board_messages", force: :cascade do |t|
-    t.text     "content"
-    t.integer  "board_user_id"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.text "content"
+    t.integer "board_user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "board_users", force: :cascade do |t|
-    t.text     "nickname"
-    t.integer  "account_id"
-    t.text     "memo"
+    t.text "nickname"
+    t.integer "account_id"
+    t.text "memo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "cards", force: :cascade do |t|
-    t.text     "title"
-    t.text     "author"
-    t.integer  "price"
-    t.text     "publisher"
-    t.text     "memo"
+    t.text "title"
+    t.text "author"
+    t.integer "price"
+    t.text "publisher"
+    t.text "memo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "data_pages", force: :cascade do |t|
-    t.text     "data"
+    t.text "data"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "messages", force: :cascade do |t|
-    t.integer  "person_id"
-    t.text     "title"
-    t.text     "message"
+    t.integer "person_id"
+    t.text "title"
+    t.text "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "mycontacts", force: :cascade do |t|
-    t.text     "name"
-    t.integer  "age"
-    t.boolean  "nationality"
-    t.text     "mail"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.text "name"
+    t.integer "age"
+    t.boolean "nationality"
+    t.text "mail"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "people", force: :cascade do |t|
-    t.text     "name"
-    t.integer  "age"
-    t.text     "mail"
+    t.text "name"
+    t.integer "age"
+    t.text "mail"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "questionaries", force: :cascade do |t|
-    t.text     "title"
-    t.text     "description"
+    t.text "title"
+    t.text "description"
     t.datetime "deadline"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "questionary_choices", force: :cascade do |t|
-    t.text     "content"
-    t.integer  "value"
-    t.integer  "questionary_item_id"
-    t.datetime "created_at",          null: false
-    t.datetime "updated_at",          null: false
+    t.text "content"
+    t.integer "value"
+    t.integer "questionary_item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "questionary_items", force: :cascade do |t|
-    t.text     "content"
-    t.integer  "questionary_id"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.text "content"
+    t.integer "questionary_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "questionary_results", force: :cascade do |t|
-    t.integer  "questionary_id"
-    t.text     "result"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.integer "questionary_item"
+    t.text "result"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "questions", force: :cascade do |t|
-    t.text     "title"
-    t.text     "content"
-    t.text     "name"
-    t.boolean  "finished"
+    t.text "title"
+    t.text "content"
+    t.text "name"
+    t.boolean "finished"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
