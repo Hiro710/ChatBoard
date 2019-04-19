35e1baf4267a937701dfe3fb6d4e94ecc8ca388c
Toropon <duck7quick@gmail.com>
2019-04-20 03:51:16 +0900

    image uploader is comments

--- app/models/board_message.rb
+++ app/models/board_message.rb
@@ -1,7 +1,7 @@
 class BoardMessage < ApplicationRecord
 	belongs_to :board_user
 
-	mount_uploader :image, ImageUploader
+#	mount_uploader :image, ImageUploader
 
 	validates :content, presence: {message:'は、必須項目です。'}
 
